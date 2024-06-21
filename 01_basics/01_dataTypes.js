b1d4d555f22e26e4eafe93fa111450f840eb0d2c
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-21 17:29:15 +0000

    datatypes and variables

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS Code as newer version
+
+//alert(3+3)  //we are using nodejs,not browser
+
+console.log(3+3);
+
+
+let name="uday"
+let age=20
+let isLoggedIn=true
+
+//primitive datatypes
+
+//number => 2 to power of 53
+//big int
+//string => " xyz"
+//boolean => true/false
+//null =>standalone value
+//undefined 
+//symbol => used in react to find uniqueness
+
+
+//object
+
+console.log(typeof"uday")
+console.log(typeof null) //object
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="uday@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2//not allowed
+console.log(accountId);
+accountEmail="xyz@gmail.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
