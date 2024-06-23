44a0d530044cf26ba8ae649642247654f0c50b97
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-23 15:52:43 +0000

    operations

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -28,7 +28,50 @@ console.log(d);
 // ""=> false
 // "hitesh" => true
 
+/*
 let n=33
 let stringNum= String(n)
 console.log(stringNum)
 console.log(typeof stringNum)
+*/
+
+//*********************** Operations *************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2+2);
+*/
+
+let str1 = "hello"
+let str2 = " github"
+
+let str3 = str1 + str2
+/*
+console.log(str3)
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log( (3+4)*5%3)
+*/
+
+//console.log(true)
+//console.log(+true)
+//console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
+
+let g=100;
+++g;
+console.log(g)
+
+
