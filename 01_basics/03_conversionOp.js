504c786bdb9b9d61242b928f9aba0e52f8e81670
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-23 12:06:35 +0000

    conversionOfDatatypes

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,34 @@
+/*
+let score="33"
+
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+//"33" =>33
+//"33abc" => NaN
+// true => 1; false =>0
+
+/*
+let isLoggedIn = 1
+let a=""
+let b="uday"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let c=Boolean(a)
+let d=Boolean(b)
+console.log(booleanIsLoggedIn);
+console.log(c);
+console.log(d);
+*/
+
+// 1 => true; 0 => false
+// ""=> false
+// "hitesh" => true
+
+let n=33
+let stringNum= String(n)
+console.log(stringNum)
+console.log(typeof stringNum)
