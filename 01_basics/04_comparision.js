b87171363801bdf9e5124dacd5702dcaa2cdbf77
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-24 08:49:52 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2==1)
+
+//console.log("2" >1)
+//console.log("02">1)
+
+console.log(null >0) //false
+console.log(null==0) //false
+console.log(null>=0) //true
+
+console.log(undefined ==0)
+console.log(undefined >=0) 
+
+//js converts null to zero thats the reason null >=0 is true
+
+// === it will check if both are same datatype
+
+console.log("2" === 2)
