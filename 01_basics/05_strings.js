cd8c98ff9ec839e69329d83f2b71fc77a585144f
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-25 17:48:27 +0000

    Strings and its operations

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name="veda"
+const count=50
+
+//console.log(name + count + "hello")
+
+console.log(`hello my name is ${name}.And my count is ${count}`)
+// String Interpolation^
+
+const gameName= new String('udaya')
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
