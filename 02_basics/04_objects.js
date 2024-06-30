c93fda0b9398dfdbea39a57d94156dee3e293971
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-30 17:44:10 +0000

    objects -2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -50,3 +50,12 @@ const users = [
         email:"sammmmm"
     }
 ]
+
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'))
