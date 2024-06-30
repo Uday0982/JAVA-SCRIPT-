2d09b7b5fd480b02739149595db4436a389ef1ec
Uday0982 <144715128+Uday0982@users.noreply.github.com>
2024-06-30 16:20:27 +0000

    object literals

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,7 +7,7 @@ const mySym = Symbol("key1")
 const Juser = {
     name: "veda",
     "fullName": "uv",
-    [mySym]:  "mykey1", //symbol
+    [mySym]:  "mykey1", 
     age:20,
     location: "tirupati",
     email: "xyz@gamil.com",
@@ -15,10 +15,27 @@ const Juser = {
     lastLoginDays: ["tuesday", "wednesday"]
 }
 
-console.log(Juser.email)
-console.log(Juser["email"])
-console.log(Juser["fullName"])
-console.log(typeof Juser[mySym])
+// console.log(Juser.email)
+// console.log(Juser["email"])
+// console.log(Juser["fullName"])
+// console.log(typeof Juser[mySym])
+
+Juser.email = "abc@gmail.com"
+// Object.freeze(Juser)
+Juser.email = "abc@abc.com"
+// console.log(Juser)
+
+Juser.greeting = function(){
+    console.log("hello evryone")
+}
+
+Juser.greeting2 = function(){
+    console.log(`hello js user, ${this.name}`)
+}
+
+
+console.log(Juser.greeting())
+console.log(Juser.greeting2())
 
 
 
